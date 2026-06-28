45c09c669cc369660566c424a739dfc6a07cc986
Jainam-Shah21 <jainamhshah0210@gmail.com>
2026-06-28 23:10:09 +0530

    Update variable_learning.py

--- Basic/variable_learning.py
+++ Basic/variable_learning.py
@@ -1,1 +1,28 @@
+print("My name is Jainam")
+print( "Jainam is learning python")
+print( "Jainam wants to become a python expert")
+
+# ----------------------------------------------------
+
+name =  input("ENTER NAME")  #using the variable if in future I want to make changes in the text I would not 
+#have to search each name and change, it only the change in the variable would apply change in 
+#the entire program this is highly effective in complex and complicated projects where ther are
+#multiple entries and the change of name would cause a mess in the entire program
+
+print("My name is ", name)
+print(name, " is learning python")
+print(name, " wants to become a python expert")
+
+
+#If you want to be seen as a professional, dont repeat words rather rename it with a variable 
+#and call the variable wherever required
+
+# ----------------------------------------------------
+#CHALLENGE OF THE DAY
+#to print the output as it is
+at = "datawithjainam.com"
+
+print ("info@", at)
+print("suppot@",at)
+print("www.",at)
 
