1848165ed986d19dedac91eaab9112311787f9e9
Jainam-Shah21 <jainamhshah0210@gmail.com>
2026-06-28 23:26:46 +0530

    Create number_mastery_2.py

---
+++ Basic/number_mastery_2.py
@@ -0,0 +1,15 @@
+#Random
+# this is also used in multiple games like online ludo 
+
+import random
+print(random.random())
+print(random.randint(1,6))
+
+#==================================
+#validation
+# to check whether the taken input is an int or not
+x=7
+y=35.5
+print(x.is_integer())
+print(y.is_integer())
+print(isinstance(x,int))
