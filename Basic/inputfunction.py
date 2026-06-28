6fbed2f7c49da95cd0d0b5b0fa3b50c533a25da3
Jainam-Shah21 <jainamhshah0210@gmail.com>
2026-06-28 23:15:51 +0530

    Create inputfunction.py

---
+++ Basic/inputfunction.py
@@ -0,0 +1,13 @@
+# Aim : Ask The user to input their name
+
+name = input("Enter your name :")
+print ("You Are ",name)
+
+# TO ask the user to input 2 random numbers to print their sum and product
+print("Print 2 integers to print their sum and product")
+x=int(input())
+y=int(input())
+add= x+y
+mult= x* y
+print("The sum is:" + str(add))
+print("The product is:" + str(mult))
